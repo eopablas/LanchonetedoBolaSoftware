5f7c44755a3a98bcd7f6e2828a26559b6f12b82f
ItsRamirez <pablo.ramirezmagalhaes12@gmail.com>
2026-08-26 21:34:46 -0300

    Enhance login form with success alert and session storage
    Added success alert and session storage for login.

--- js/script.js
+++ js/script.js
@@ -37,5 +37,9 @@ formLogin.addEventListener("submit", function (event) {
     localStorage.setItem("nomeUsuario", nome);
     localStorage.setItem("emailUsuario", email);
 
+    alert("Login realizado com sucesso!");
+
+    sessionStorage.setItem("logado", "true");
+
     window.location.href = "pages/home.html";
 });
